3232a2c2efab1383b4b08b3cd6897cfcc1d5cdc8
pavel.martovskiy <pavel.martovskiy@n-cube.co.uk>
2018-09-27 10:19:25 +0300

    Added start start same activity

--- app/src/main/java/com/pm/dagger212/main/MainActivity.kt
+++ app/src/main/java/com/pm/dagger212/main/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.pm.dagger212.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import com.pm.dagger212.R
 import com.pm.dagger212.R.layout
 import com.pm.dagger212.app.AppModel
 import com.pm.dagger212.app.AppModel2
 import com.pm.dagger212.app.IAppModel2
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.view.btNewScreen
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -24,5 +28,12 @@ class MainActivity : DaggerAppCompatActivity() {
     model.printLogMessage()
     appModel.printLogMessage("MAIN ACTIVITY")
     appModel2.printLogMessage("MAIN ACTIVITY")
+
+    this
+        .findViewById<View>(R.id.btNewScreen)
+        .setOnClickListener {
+          startActivity(Intent(this, MainActivity::class.java))
+        }
+
   }
 }
